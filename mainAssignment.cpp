cba127a5d6d5d8652a2777a60ee2399e0e435e74
minthukha-coder <112501430+minthukha-coder@users.noreply.github.com>
2022-12-02 01:03:43 +0630

    Create mainAssignment.cpp

---
+++ mainAssignment.cpp
@@ -0,0 +1,13 @@
+//
+// Created by User on 23/11/2022.
+//
+#include"iostream"
+#include"bankassignment.h"
+using namespace std;
+using namespace M;
+
+int main(){
+    bank obj;
+    obj.mainLoading();
+    obj.mainMenu();
+}
